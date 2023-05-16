0bec2e3adb7afeede619a299702615c71ac0eb78
Ivan Gerardo Lomeli Reyna <114118249+IvanG2002@users.noreply.github.com>
2023-05-16 11:38:45 -0600

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+type FormElement = React.FormEvent<HTMLFormElement>;
+interface ITask {
+  name: string;
+  done: boolean;
+}
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState<string>("");
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const taskInput = useRef<HTMLInputElement>(null)
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault();
+    addTask(newTask);
+    setNewTask("");
+    taskInput.current?.focus()
+  };
+  const addTask = (name: string): void => {
+    const newTask: ITask[] = [...tasks, { name, done: false }];
+    setTasks(newTask);
+  };
 
-function App() {
+  const toggleDoneTask = (i: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks[i].done = !newTasks[i].done;
+    setTasks(newTasks);
+  };
+  const removeTask = (i: number): void => {
+    const newTasks: ITask[] = [...tasks]
+    newTasks.splice(i,1)
+    setTasks(newTasks)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => {
+            setNewTask(e.target.value);
+          }}
+          value={newTask}
+          ref={taskInput}
+          autoFocus
+        />
+        <button>Save</button>
+      </form>
+      {tasks.map((task: ITask, index: number) => {
+        return (
+          <div key={index}>
+            <h2 style={{ textDecoration: task.done ? "line-through" : "" }}>
+              {task.name}
+            </h2>
+            <div>
+              <button onClick={() => toggleDoneTask(index)}>
+                {task.done ? "✓" : "✗"}
+              </button>
+              <button onClick={() => removeTask(index)}>🗑</button>
+            </div>
+            <p>{task.done + ""}</p>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
